7f19de7f5387ac6666adc1ad8bf0f1d01b38abbc
FilipS90 <filip.stojiljkovic90@yahoo.com>
2021-11-25 15:04:09 +0100

    commit

--- src/main/java/guru/springpetclinic/SpringPetClinicApplication.java
+++ src/main/java/guru/springpetclinic/SpringPetClinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPetClinicApplication {
 	
-	// bla
+	// blaasda
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPetClinicApplication.class, args);
